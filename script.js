648fc1d226d11baa4eaea7712008a327138ffbbc
mfg25 <unnmfg@usp.br>
2023-02-24 21:52:11 -0300

    caesar cipher analyze array

--- script.js
+++ script.js
@@ -19,3 +19,55 @@ export let calculator = {
         return a * b
     }
 }
+
+export function reverseString(string){
+    if(string.length == 1){
+        return string
+    }else{
+        let array = string.split('')
+        let letter = array.shift()
+        
+        return reverseString(array.join('')) + letter
+    }
+}
+
+export function analyzeArray(array){
+    let max = array[0]
+    let min = array[0]
+    let average = 0
+    for (const e of array) {
+        if(e > max) max = e
+        if(e < min) min = e
+        average += e
+    }
+    average = average/array.length
+    return {average: average, min: min, max: max, length: array.length}
+}
+
+export function caesarCipher(string, shift){
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let alphabetObj = {}
+    let i = 0
+    for(let e of alphabet){
+        alphabetObj[e] = {e, value: i}
+        i++
+    }
+    let splitedArray = string.split('')
+    let result = []
+   
+    for(let letter of splitedArray){
+        for(let letterProp in alphabetObj){
+            if(alphabetObj[letterProp].e == letter){
+                for(let letterObj1 in alphabetObj){
+                    let finalValue = (alphabetObj[letterProp].value + shift) % 26
+                    if(alphabetObj[letterObj1].value == finalValue) result.push(alphabetObj[letterObj1].e)
+                }
+            }
+        }
+    }
+    
+    return result.join('')
+}
+for(let i = 0; i < 25; i++){
+    console.log(caesarCipher('teteu', i))
+}
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import {capitalize, calculator} from './script.js'
+import {capitalize, calculator, reverseString, analyzeArray, caesarCipher} from './script.js'
 
 it('capitalize first letter', () => {
     expect(capitalize('teste')).toBe('Teste')
@@ -9,4 +9,21 @@ it('calculator', () => {
     expect(calculator.subtract(2, 2)).toBe(0)
     expect(calculator.divide(2, 2)).toBe(1)
     expect(calculator.multiply(2, 2)).toBe(4)
+})
+
+it('reverse string', () =>{
+    expect(reverseString('banana')).toBe('ananab')
+    expect(reverseString('1234')).toBe('4321')
+})
+
+it("analyze array", () =>{
+    expect(analyzeArray([1,8,3,4,2,6])).toStrictEqual({ average: 4, min: 1, max: 8, length: 6 })
+    expect(analyzeArray([10,0,0,0])).toStrictEqual({ average: 2.5, min: 0, max: 10, length: 4 })
+})
+
+it("caesar cipher", () =>{
+    expect(caesarCipher('teste', 5)).toBe('yjxyj')
+    expect(caesarCipher('zzz', 1)).toBe('aaa')
+    expect(caesarCipher('matheus', 20)).toBe('gunbyom')
+    
 })
